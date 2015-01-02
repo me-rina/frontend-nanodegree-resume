a94fc8822d5c3f0a240cd2dd491d18b164aca686
me-rina <amr@outsidethepond.com>
2015-01-02 17:49:18 -0500

    going to dinner
    good enough for now

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -16,20 +16,32 @@ console.log(at);
 // console.log(newat);
 console.log("This is B4",HTMLheaderName);
 */
-var skills = ["SQL, ","Support, ","Programming, ","Other Stuff "]
+var skills = [HTMLskills.replace("%data%","SQL"),
+ HTMLskills.replace("%data%","Support"),
+ HTMLskills.replace("%data%","Programming"),
+ HTMLskills.replace("%data%","Other Stuff ")];
+ 
 var formattedName=HTMLheaderName.replace("%data%","Anne Marie Rossi");
 var formattedRole=HTMLheaderRole.replace("%data%","Factotum");
 var bio = {
 //key: value pairs
 "name": formattedName,
 "role": formattedRole,
-"contactGeneric": HTMLcontactGeneric.replace("%contact%","Call me ").replace("%data%","+1 973.743.5968"),
+"contact": 
+    {"email": HTMLemail.replace("%data%","amr@outsidethepond.com"),
+    "blog": HTMLblog.replace("%data%","outsidethepond.com"),
+    "location": HTMLlocation.replace("%data%","07003")},
 "bioPic":HTMLbioPic.replace("%data%","images/me.gif"),
 "skills":skills,
 "welcomeMsg":HTMLWelcomeMsg.replace("%data%","What do I do? ")
 };
+bio.contactGeneric=HTMLcontactGeneric.replace("%contact%","").replace("%data%",bio.contact)
+$("#header").append(bio.name).append(bio.role);
+$("#topContacts").append(bio.contact.email).append(bio.contact.blog).append(bio.contact.location);
 
-$("#header").append(bio.name).append(bio.role).append(" ").append(bio.contactGeneric).append(bio.welcomeMsg).append(bio.skills).append(bio.bioPic);
+
+$("#header").append(bio.bioPic).append(bio.welcomeMsg).append(bio.skills);
+$("#footerContacts").append(bio.contact.email).append(bio.contact.blog).append(bio.contact.location);
 
 
 
